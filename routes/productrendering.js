9a44141d128fc997055195cbf3cf141bb4d11736
UnmeshDeshmukh <unmesh.deshmukh@sjsu.edu>
2016-05-07 02:20:15 -0700

    Jeans and Hood pages

--- routes/productrendering.js
+++ routes/productrendering.js
@@ -27,3 +27,47 @@ exports.renderMenTeesPage=function(req,res){
 	});
 };
 
+
+exports.getProductsMenJeans = function(req, res) {
+	var email = req.session.email;	
+	var str='http://team4praj-env.us-west-2.elasticbeanstalk.com/getProducts/MEN/Jeans';
+	var httpcall = request('GET', str, {
+		 'headers': {
+			    'user-agent': 'example-user-agent'
+			  }
+	});
+			
+	console.log(httpcall.getBody('utf8'));
+	res.send(httpcall.getBody('utf8'));
+}
+
+
+exports.renderMenJeansPage=function(req,res){
+	ejs.renderFile("./views/JeansProducts.ejs", function(err, result) {
+		if (!err) {			
+			res.end(result);
+		}
+	});
+};
+
+exports.getProductsMenHoods = function(req, res) {
+	var email = req.session.email;	
+	var str='http://team4praj-env.us-west-2.elasticbeanstalk.com/getProducts/MEN/Hoods';
+	var httpcall = request('GET', str, {
+		 'headers': {
+			    'user-agent': 'example-user-agent'
+			  }
+	});
+			
+	console.log(httpcall.getBody('utf8'));
+	res.send(httpcall.getBody('utf8'));
+}
+
+
+exports.renderMenHoodsPage=function(req,res){
+	ejs.renderFile("./views/HoodsProducts.ejs", function(err, result) {
+		if (!err) {			
+			res.end(result);
+		}
+	});
+};
